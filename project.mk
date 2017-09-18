be834530e961ed1b44bfac20ffb65bd1c303d763
Ronaldo Faria Lima <ronaldo@nineteen.com.br>
2017-09-17 21:59:40 -0300

    Cleaned up project.mk file

--- project.mk
+++ project.mk
@@ -25,47 +25,6 @@
 # (C) 2017 - Ronaldo Faria Lima
 #
 
-# This was created with git submodules in mind. So, MODULES variable contains a
-# list of names of modules used by your project. Your modules must be placed on
-# Modules directory. If you want to place your modules someplace else, you can
-# override here the DEPS variable. See main Makefile to see how it is
-# created. But edit it here.
-MODULES=
-
-# This is the xcodeproj bundle for your project. 
 PROJECT=Themify.xcodeproj
-
-# This is the scheme you want to build from command line
 SCHEME="Themify"
-
-# This is the target you want to build from command line
 TARGET=Themify
-
-#
-# Overrides
-#
-
-# Uncomment below if you want to build on Debug mode. By default, the main
-# makefile builds using Release configuration.
-
-# CONFIGURATION=Debug
-
-# Uncomment and edit below if you want to have your submodules placed on a
-# directory different from Modules. Replace the Modules/% to MyPlace/% to setup
-# the correct directory.
-
-# DEPS=${MODULES:%=Modules/%}
-
-#
-# Uncomment and edit below to customize where your derived data will
-# reside. CAREFUL: the clean-all target will recursively delete this directory.
-#
-
-# BUILDDIR=build
-
-#
-# Uncomment and edit below to customize Xcode's destination settings. By
-# default, it uses the generic setting for iOS compilation.
-#
-
-# DESTINATION="generic/platform=iOS"
